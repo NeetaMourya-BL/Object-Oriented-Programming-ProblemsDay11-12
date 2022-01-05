60379b50d93313b70583d08e8eccdebd56c1b2e8
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-01-05 17:08:34 +0530

    added in deck of card linked list

---
+++ src/main/java/com/bridgelabz/deckofcards/DeckOfCardsLinkedList.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.deckofcards;
+
+public class DeckOfCardsLinkedList {
+	class Node{
+	    String data;
+	    Node addressOfNextNode;
+	   
+	public Node(String data) {
+		this.data = data;
+		this.addressOfNextNode = null;
+	   }     
+	}
+}
