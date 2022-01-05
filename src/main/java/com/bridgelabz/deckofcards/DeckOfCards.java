15c8c1b7b125f6145c590cfaa982a5370fbce499
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-01-05 17:03:06 +0530

    added in deck of cards

--- src/main/java/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/main/java/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,68 +1,76 @@
 package com.bridgelabz.deckofcards;
 
-public class DeckOfCards {
-	private String suits;
-	private String ranks;
-	private static final int numOfPlayer = 4;
-    private static final int numOfCardByPlayer = 9;
-	private static final int deckOfCard = 0;
-	int[][] numOfCards = new int[4][9];
-   public static void main(String[] args) {
-	int[] numOfCards = new int[52];
-	
-	    // you loop as much as the number of players
-	    // your increment step is the number of cards to deal by player
-	    // your end condition is the number of cards you have to
-	    // deal for all player
-	System.out.println("Welcome to deck of cards");
-	DeckOfCards cards=new DeckOfCards();
-	cards.initialGame();
-	cards.printCard();
-}
-// initialGame method for Setup the initial game with deck of cards and make sure we have unique cards
-public void initialGame() {
-	 int playerNumber = 1;
-    String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
-    String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-    // initialize deck
-    int n = suits.length * ranks.length;
-    String[] deck = new String[n];
-    for (int i = 0; i < ranks.length; i++) {
-        for (int j = 0; j < suits.length; j++) {
-            deck[suits.length*i + j] = ranks[i] + " of " + suits[j];
-        }
-    }
-    // shuffle
-    for (int i = 0; i < n; i++) {
-        int r = i + (int) (Math.random() * (n-i));
-        String temp = deck[r];
-        deck[r] = deck[i];
-        deck[i] = temp;
-    }
-    for (int i = 0; i < numOfPlayer * numOfCardByPlayer; i = i + numOfCardByPlayer) {
-        System.out.println("player " + playerNumber + ", cards =");
-        // you loop as much as the number of cards to deal by player
-        // your start from the last dealed card 
-        // your increment step is 1 as you want to deal card by card from the deck
-        // your end condition is the number of cards to deal by player
-        for (int j = i; j < i+numOfCardByPlayer; j++) {
-            System.out.println(deck[j]);
-        }
-        playerNumber++;
-}
-}
-public void printCard() {
-    for(int i = 0; i<numOfPlayer; i++)
-        for(int j = 0; j<numOfCardByPlayer; j++)
-        	numOfCards[i][j] = 0;
+/*
+ * Deck Of Cards Program
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    for(int i = 0; i<numOfPlayer; i++)
-    {
-        for(int j = 0; j<numOfCardByPlayer; j++)
-        {
-            System.out.print(numOfCards[i][j]);
-        }
-        System.out.println();
-    }
-}
+public class DeckOfCards {
+	static final int players = 4;
+	static final int cards_assigned = 9;
+	String[] decks = new String[52];
+	String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+	String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+	int[][] pCards = new int[4][13];
+	int p = 0;
+
+	int random = (int) (Math.random() * pCards.length);
+
+	/*
+	 * / Deck Card Creation
+	 */
+	public void decksBuild() {
+		int k = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 13; j++) {
+				decks[k] = suits[i] + " of " + ranks[j];
+				k++;
+			}
+		}
+		System.out.println();
+	}
+
+	/*
+	 *  show the decks created by decksBuild() method
+	 */
+	public void showDecks() {
+		System.out.println("Decks Create:" + Arrays.asList(decks));
+	}
+
+	/*
+	 * / Shuffle Cards
+	 */
+	public void shuffleCards() {
+		for (int i = 0; i < pCards.length; i++) {
+			for (int j = 0; j < pCards[i].length; j++) {
+				// pCards[i][j] = decks[k];
+				pCards[i][j] = random;
+			}
+		}
+		System.out.println();
+	}
+
+	/*
+	 * / Print assigned cards for 4 player
+	 */
+	public void displayPlayerCards() {
+		for (int i = 0; i < 4; i++) {
+			System.out.println("Player" + i);
+			for (int j = 0; j < 9; j++) {
+				System.out.println("Assign Card to player:" + decks[p]);
+				p++;
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		DeckOfCards deckCard = new DeckOfCards();
+		deckCard.decksBuild();
+		deckCard.showDecks();
+		deckCard.shuffleCards();
+		deckCard.displayPlayerCards();
+
+	}
 }
